a26fd9aff765a7434fffca10edf69f8ebb85616c
Hong Jimin <76777722+jiiminhong@users.noreply.github.com>
2024-03-24 01:07:58 +0900

    [level 0] Title: 문자열 섞기, Time: 0.26 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cs
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cs
@@ -4,9 +4,8 @@ public class Solution {
     public string solution(string str1, string str2) {
         string answer = "";
         
-        for (int i = 0; i < str2.Length; i++) {
-            answer += str1[i];
-            answer += str2[i];
+        for (int i = 0; i < st2.Length; i++) {
+            answer = answer + str1[i] + str2[i];
         }
         
         return answer;
