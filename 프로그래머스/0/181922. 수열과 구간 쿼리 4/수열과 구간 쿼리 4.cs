b45a35dcaeb0b7fd0cffdb66b5e3cb15a862eab7
Hong Jimin <76777722+jiiminhong@users.noreply.github.com>
2024-04-15 12:22:50 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 0.68 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+        // int[] answer = new int[] {};
+        
+        for (int i = 0; i < queries.GetLength(0); i++) {
+            for (int j = queries[i, 0]; j <= queries[i, 1]; j++) {
+                if (j % queries[i, 2] == 0)
+                    arr[j]++;
+            }
+        }
+        return arr;
+    }
+}
